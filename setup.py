9b6d07918f05c2a2e9834f75702e2907b41f518e
Mariana Esther <mar.esther23@gmail.com>
2020-03-06 13:43:59 -0600

    format

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
   description = 'Python library to create equivalence dictionaries between a set of texts and a catalog using FuzzyWuzzy. ',
   author = 'ME Martinez-Sanchez',
   author_email = 'mar.esther23@gmail.com',
-  url = 'https://github.com/mar-esther23/WibblyWobbly',
   download_url = 'https://github.com/mar-esther23/WibblyWobbly/archive/v_01.tar.gz',
   keywords = ['fuzzy matching', 'catalog', 'data cleaning', 'fuzzywuzzy'],
   install_requires=[
@@ -16,8 +15,6 @@ setup(
           'pandas',
       ],
   classifiers=[
-    'Development Status :: 3 - Beta',
-    'Intended Audience :: DataScientists',
     'Topic :: Data Science :: Data cleaning',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3',
